fd4e6e2bd3266f9e688da571c3ad3522acaecc06
johnsila <60229418+johnsila@users.noreply.github.com>
2020-02-06 19:42:24 +0300

    Add files via upload

---
+++ stringOperators.py
@@ -0,0 +1,18 @@
+#string operators
+print("string operators")
+a="hello"
+b="world"
+#concatination
+print("1.",a+b)
+
+#print a 5times
+print("2.",a*5)
+
+#bool. is there h in a
+print("3.",'h'in a)
+
+#bool. is there m in a
+print("4.",'m'in a)
+
+#bool. is x not in a
+print("5.",'x'not in a)
---
+++ strings.py
@@ -0,0 +1,26 @@
+a="hello world"
+#print entire characters string
+print("1.",a[:])
+
+#print first 6 characters of the string
+print("2.",a[:6])
+
+#count the first character of the string
+print("3.",a[0])
+
+#print the 3rd to 4th characters of the string(slicing)
+print("4.",a[2:5])
+
+#print last character of the string
+print("5.",a[-1])
+
+#print entire string
+print("6.",a[0:])
+
+#white space is counted as a character
+print("7.",a[5])
+
+#print the 7th to 9th characters of the string(slicing)
+print("8.",a[6:10])
+
+
